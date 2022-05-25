41d1e2f66aa5a343ffd54643098a08ec1aeb6298
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2022-05-25 14:30:46 +0200

    Added a way to see how many resources you end up with

--- Ratio calc.py
+++ Ratio calc.py
@@ -1,10 +1,10 @@
 import random
 import statistics
 
-starting_resources = int(input())
-d = int(input())
-s = int(input())
-xp = int(input())
+starting_resources = int(input("Starting resources: "))
+double = int(input("Doubling chance: "))
+save = int(input("Saving chance: "))
+xp = int(input("XP per action: "))
 ITERATIONS = 1000
 total_list = []
 total_xp_list = []
@@ -14,13 +14,13 @@ total_xp = 0
 for i in range(ITERATIONS):
     processed_resources = 0
     while starting_resources > 0:
-        double = random.randint(1, 100)
-        if double <= d:
+        double_roll = random.randint(1, 100)
+        if double_roll <= double:
             processed_resources += 2
         else:
             processed_resources += 1
-        save = random.randint(1, 100)
-        if save <= s:
+        save_roll = random.randint(1, 100)
+        if save_roll <= save:
             starting_resources += 1
             total_xp += xp
         starting_resources -= 1
@@ -31,5 +31,6 @@ for i in range(ITERATIONS):
         total_xp = 0
     total_list.append(processed_resources)
 
-print("Resource gains: ", statistics.mean(total_list) - starting_resources)
+print("% resource increase from base ", statistics.mean(total_list) - starting_resources)
+print("Resources produced: ", statistics.mean(total_list))
 print("XP gained: ", statistics.mean(total_xp_list))
