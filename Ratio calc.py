5c2537b455cedb6ca82b95cc545d2f1e5be83137
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2022-05-28 12:55:45 +0200

    Fixed resources resetting to 100 every iteration, now resets to input value

--- Ratio calc.py
+++ Ratio calc.py
@@ -2,14 +2,16 @@ import random
 import statistics
 
 starting_resources = int(input("Starting resources: "))
+r_cycle = starting_resources
 double = int(input("Doubling chance: "))
 save = int(input("Saving chance: "))
 xp = int(input("XP per action: "))
-ITERATIONS = 1000
+#crafting_time = float(input())
+ITERATIONS = 10
 total_list = []
 total_xp_list = []
 total_xp = 0
-
+crafting_cycles = 0
 
 for i in range(ITERATIONS):
     processed_resources = 0
@@ -24,13 +26,16 @@ for i in range(ITERATIONS):
             starting_resources += 1
             total_xp += xp
         starting_resources -= 1
+        crafting_cycles += 1
         total_xp += xp
     if starting_resources == 0:
         total_xp_list.append(total_xp)
-        starting_resources = 100
+        starting_resources = r_cycle
         total_xp = 0
     total_list.append(processed_resources)
 
-print("% resource increase from base ", statistics.mean(total_list) - starting_resources)
-print("Resources produced: ", statistics.mean(total_list))
-print("XP gained: ", statistics.mean(total_xp_list))
+
+print("% resource increase from base ", round((statistics.mean(total_list) - starting_resources), 2))  # fix this line to show % instead of difference
+print("Resources produced: ", round((statistics.mean(total_list)), 2))
+print("XP gained: ", round(statistics.mean(total_xp_list)), 2)
+#print("Crafting cycles: ")
